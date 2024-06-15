ed837d0aaa7221c4f6b91dd5264a5179927cfb22
Shohidur Rahman <s.rahmanrahi15@gmail.com>
2024-06-15 21:37:30 +0600

    fixed

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import express, { json } from "express";
+const express = require("express");
 const app = express();
-import cors from "cors";
-import { MongoClient } from "mongodb";
-import { ObjectId } from "mongodb";
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
-app.use(json({ limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
 
 app.get("/", (req, res) => {
   res.send("Running UGoOnTravel Server");
