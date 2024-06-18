d0a933f81f6954dc240e70f7115aff7030022d0e
Shohidur Rahman <s.rahmanrahi15@gmail.com>
2024-06-18 20:23:24 +0600

    fixed

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const ObjectId = require("mongodb").ObjectId;
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 //middleware
-app.use(cors());
+app.use(cors({
+  origin:[
+    "http://localhost:3000/",
+    "https://ugoontravel-43d92.web.app/"
+  ]
+}));
 app.use(express.json({ limit: "50mb" }));
 
 app.get("/", (req, res) => {
